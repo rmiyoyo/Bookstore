9cc035cb4f35e0816a899dc9f770b54bb73374db
rmiyoyo <miyoyoraphael@gmail.com>
2023-07-18 23:36:55 +0300

    Add boilerplate

--- Main.hs
+++ Main.hs
@@ -1,1 +1,8 @@
-main :: IO ()
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+data Book = Book
+ { bookTitle::String
+    , bookAuthor::String
+    , bookPrice:: Double
+ } deriving Show()
